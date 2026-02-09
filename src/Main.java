c80db2d186745c4561ea769e3cd66fd0353da513
deeksha-sn <deekshasn18@gmail.com>
2026-02-09 21:17:37 +0530

    Initial project setup with Main.java

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Fraud Detection System - Project Setup Complete!");
+        System.out.println("Ready to build!");
+    }
+}
