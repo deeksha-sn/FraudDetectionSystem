3461f7fc7a9f3e5f802c861d4223c6606dacc9a6
deeksha-sn <deekshasn18@gmail.com>
2026-02-19 20:57:59 +0530

    error corrected AlertService.java

--- src/services/AlertService.java
+++ src/services/AlertService.java
@@ -1,10 +1,5 @@
 package services;
 
-public class AlertService {
-    
-}
-package services;
-
 import models.Alert;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,11 +24,11 @@ public class AlertService {
             System.out.println("\n✅ No fraud alerts - All transactions are clean!");
             return;
         }
-        
+
         System.out.println("\n" + "=".repeat(65));
         System.out.println("📢 FRAUD ALERT SUMMARY - Total Alerts: " + alerts.size());
         System.out.println("=".repeat(65));
-        
+
         for (Alert alert : alerts) {
             System.out.println(alert);
         }
